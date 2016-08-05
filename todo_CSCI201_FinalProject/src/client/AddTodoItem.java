cbe7b2d124f40c38a1c2d9926f2a955b7ec08db7
Alexander Kingsborough <kingsbor@usc.edu>
2016-08-04 17:04:50 -0700

    todo class

--- todo_CSCI201_FinalProject/src/client/AddTodoItem.java
+++ todo_CSCI201_FinalProject/src/client/AddTodoItem.java
@@ -18,6 +18,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
@@ -36,6 +37,7 @@ public class AddTodoItem extends JFrame {
 	private static final long serialVersionUID = 1376543;
 	JLabel mTitleLabel;
 	JLabel mPriorityLabel;
+	JLabel mPointsLabel;
 	JLabel mPrivacyLabel;
 	JLabel mListLabel;
 	JLabel mDescriptionLabel;
@@ -43,7 +45,7 @@ public class AddTodoItem extends JFrame {
 	JTextField mTitleText;
 	JRadioButton mPublicRB;
 	JTextArea mDescriptionText;
-	JComboBox<String> mPriorityBox;
+	JComboBox<Integer> mPriorityBox;
 	JComboBox<String> mListBox;
 	JRadioButton mPrivateRB;
 	JPanel mMainPanel;
@@ -52,9 +54,11 @@ public class AddTodoItem extends JFrame {
 	JPanel mPrivacyPanel;
 	JPanel mListPanel;
 	JPanel mDescriptionPanel;
-	Vector<String> mPriorityVector;
+	Vector<Integer> mPriorityVector;
 	Vector<String> mListVector;
 	boolean isPrivate;
+	JTextField mPointsText;
+	Font mFont;
 	
 	public AddTodoItem(){
 		super("Add Todo");
@@ -69,48 +73,49 @@ public class AddTodoItem extends JFrame {
 	}
 	
 	private void AddTodo() {
+		mFont = new Font("Serif", Font.PLAIN, 22);
 		mMainPanel = new JPanel();
 		mTitleLabel = new JLabel("Title: ");
-		mTitleLabel.setFont(new Font("Serif", Font.PLAIN, 22));
-
+		mTitleLabel.setFont(mFont);
 		mPriorityLabel = new JLabel("Priority: ");
-		mPriorityLabel.setFont(new Font("Serif", Font.PLAIN, 22));
+		mPriorityLabel.setFont(mFont);
+		mPointsLabel = new JLabel ("Points: ");
+		mPointsLabel.setFont(mFont);
 		mPrivacyLabel = new JLabel("Privacy: ");
-		mPrivacyLabel.setFont(new Font("Serif", Font.PLAIN, 22));
+		mPrivacyLabel.setFont(mFont);
 		mListLabel = new JLabel("List: ");
-		mListLabel.setFont(new Font("Serif", Font.PLAIN, 22));
+		mListLabel.setFont(mFont);
 		mDescriptionLabel = new JLabel("Description: ");
-		mDescriptionLabel.setFont(new Font("Serif", Font.PLAIN, 22));
+		mDescriptionLabel.setFont(mFont);
 		mSaveButton = new JButton("Save");
-		mSaveButton.setFont(new Font("Serif", Font.PLAIN, 20));
+		mSaveButton.setFont(mFont.deriveFont(20));
 		mPublicRB = new JRadioButton("Public");
-		mPublicRB.setFont(new Font("Serif", Font.PLAIN, 20));
+		mPublicRB.setFont(mFont.deriveFont(20));
 		mPrivateRB = new JRadioButton("Private");
-		mPrivateRB.setFont(new Font("Serif", Font.PLAIN, 20));
+		mPrivateRB.setFont(mFont.deriveFont(20));
 		mTitleText = new JTextField(15);
-		mTitleText.setFont(new Font("Serif", Font.PLAIN, 20));
+		mTitleText.setFont(mFont.deriveFont(20));
 		mDescriptionText = new JTextArea(5, 15);
-		mDescriptionText.setFont(new Font("Serif", Font.PLAIN, 12));
-		mPriorityVector = new Vector<String>();
-		mPriorityVector.add("Top");
-		mPriorityVector.add("High");
-		mPriorityVector.add("Medium");
-		mPriorityVector.add("Low");
-		mPriorityVector.add("Lowest");
+		mDescriptionText.setFont(mFont.deriveFont(0, 10));
+		mPriorityVector = new Vector<Integer>();
+		for(int i = 10; i > 0; i--){
+			mPriorityVector.addElement(i);
+		}
+		mPointsText = new JTextField(15);
 		mListVector = new Vector<String>();
 		mListVector.add("Public Todos");
 		mListVector.add("Private Todos");
-		mPriorityBox = new JComboBox<String>(mPriorityVector);
-		mPriorityBox.setFont(new Font("Serif", Font.PLAIN, 20));
+		mPriorityBox = new JComboBox<Integer>(mPriorityVector);
+		mPriorityBox.setFont(mFont.deriveFont(20));
 		mListBox = new JComboBox<String>(mListVector);
-		mListBox.setFont(new Font("Serif", Font.PLAIN, 20));
+		mListBox.setFont(mFont.deriveFont(2));
 		mMainPanel =  new JPanel();
 		mTitlePanel =  new JPanel();
 		mPriorityPanel =  new JPanel();
 		mPrivacyPanel =  new JPanel();
 		mListPanel =  new JPanel();
 		mDescriptionPanel =  new JPanel();
-		mMainPanel.setLayout(new GridLayout(5, 2));
+		mMainPanel.setLayout(new GridLayout(6, 2));
 		mTitlePanel.setLayout(new FlowLayout());
 		mPrivacyPanel.setLayout(new FlowLayout());
 		mPrivacyPanel.add(mPublicRB);
@@ -121,7 +126,10 @@ public class AddTodoItem extends JFrame {
 		
 		mMainPanel.add(mPriorityLabel);
 		mMainPanel.add(mPriorityBox);
-
+		
+		mMainPanel.add(mPointsLabel);
+		mMainPanel.add(mPointsText);
+		
 		mMainPanel.add(mPrivacyLabel);
 		mMainPanel.add(mPrivacyPanel);
 
@@ -136,6 +144,7 @@ public class AddTodoItem extends JFrame {
 		outsidePanel.add(mMainPanel, BorderLayout.CENTER);
 		outsidePanel.add(mSaveButton, BorderLayout.SOUTH);
 		add(outsidePanel);
+		addSaveEvents();
 	}
 	
 	public static void main(String [] args){
@@ -169,15 +178,40 @@ public class AddTodoItem extends JFrame {
 	private void addSaveEvents(){
 		mSaveButton.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent ae){
+				
+				if(!isInteger(mPointsText.getText())){
+					System.out.println("in if");
+					JOptionPane.showMessageDialog(
+							null,
+						    "Please Enter a Number in the Points Field",
+						    "Error Getting Points",
+						    JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				
 				String title = mTitleText.getText();
-				String priority = mPriorityBox.getSelectedItem().toString();
+				int priority = Integer.parseInt(mPriorityBox.getSelectedItem().toString());
 				String list = mListBox.getSelectedItem().toString();
 				String description = mDescriptionText.getText();
-				int points = 0;//TODO ALEX I DIDN't want to mess with all your implementation, just
+				
+				int points = Integer.parseInt(mPointsText.getText()); ;// ALEX I DIDN't want to mess with all your implementation, just
+				
 				TodoObject mTodoObject = new TodoObject(title, priority, isPrivate, list, description, points);
 				//need to send this to the client to add to user's todos
 			}
 		});
 	}
 
+	
+	public static boolean isInteger(String s) {
+	    try { 
+	        Integer.parseInt(s); 
+	    } catch(NumberFormatException e) { 
+	        return false; 
+	    } catch(NullPointerException e) {
+	        return false;
+	    }
+	    // only got here if we didn't return false
+	    return true;
+	}
 }
