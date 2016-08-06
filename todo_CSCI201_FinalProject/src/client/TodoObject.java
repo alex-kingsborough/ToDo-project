7438ada7dee39fa3cea124e143d111adad9a2023
Lucas Johnson <Lucas Johnson>
2016-08-06 16:56:22 -0700

    stop fucking with the TodoObject

--- todo_CSCI201_FinalProject/src/client/TodoObject.java
+++ todo_CSCI201_FinalProject/src/client/TodoObject.java
@@ -6,17 +6,18 @@ public class TodoObject {
 	private int priority;
 	private boolean isPrivate;
 	private boolean isCompleted;
-	//private int listID;
+	private int listID;
 	private String listName;
 	private String desc;
 	private int mPoints;
 	private int userID;
 	
-	public TodoObject(String _title, int _priority, boolean _isPrivate, String _list, String _desc, int _points, int _userID){
+	public TodoObject(String _title, int _priority, boolean _isPrivate, int _listID, String _listName, String _desc, int _points, int _userID){
 		title = _title;
 		priority = _priority;
 		isPrivate = _isPrivate;
-		listName = _list;
+		listID = _listID;
+		listName = _listName;
 		desc = _desc;
 		mPoints = _points;
 		userID = _userID;
