92fed776a72f9afb384e61695e014e61354e69c1
Forrest Dunlap <fdunlap@>
2016-08-06 17:54:48 -0700

    Got rid of some unused imports.

--- todo_CSCI201_FinalProject/src/client/TodoFrame.java
+++ todo_CSCI201_FinalProject/src/client/TodoFrame.java
@@ -2,15 +2,10 @@ package client;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
 import java.util.Vector;
 
 import javax.swing.JFrame;
-import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.KeyStroke;
 
 public class TodoFrame extends JFrame implements Navigator {
 	
--- todo_CSCI201_FinalProject/src/server/ScrollTextPane.java
+++ todo_CSCI201_FinalProject/src/server/ScrollTextPane.java
@@ -1,29 +1,10 @@
 package server;
 
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.Rectangle;
-import java.awt.geom.AffineTransform;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.text.Document;
-import javax.swing.event.UndoableEditEvent;
-import javax.swing.event.UndoableEditListener;
-import javax.swing.plaf.basic.BasicScrollBarUI;
-import javax.swing.undo.CannotRedoException;
-import javax.swing.undo.CannotUndoException;
 import javax.swing.undo.UndoManager;
-import javax.swing.undo.UndoableEdit;
 
 public class ScrollTextPane extends JScrollPane {
 	/**
