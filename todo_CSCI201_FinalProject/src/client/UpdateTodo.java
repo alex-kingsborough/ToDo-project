33614d7bf80478d7ef91ceed0fb215f915732cba
Alexander Kingsborough <kingsbor@usc.edu>
2016-08-07 13:30:49 -0700

    coloring

--- todo_CSCI201_FinalProject/src/client/UpdateTodo.java
+++ todo_CSCI201_FinalProject/src/client/UpdateTodo.java
@@ -1,6 +1,7 @@
 package client;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -65,41 +66,100 @@ public class UpdateTodo extends JFrame {
 	private void AddTodo(TodoObject to) {
 		mFont = new Font("Serif", Font.PLAIN, 22);
 		mMainPanel = new JPanel();
+		mMainPanel.setBackground(Constants.greyColor);
+		setBackground(Constants.goldColor);
+
+		
 		mTitleLabel = new JLabel("Title: ");
 		mTitleLabel.setFont(mFont);
+		mTitleLabel.setForeground(Constants.goldColor);
+		mTitleLabel.setBackground(Constants.greyColor);		
+		
+		
 		mPriorityLabel = new JLabel("Priority: ");
 		mPriorityLabel.setFont(mFont);
+		mPriorityLabel.setForeground(Constants.goldColor);
+		mPriorityLabel.setBackground(Constants.greyColor);		
+		
+		
 		mPointsLabel = new JLabel ("Points: ");
 		mPointsLabel.setFont(mFont);
+		mPointsLabel.setForeground(Constants.goldColor);
+		mPointsLabel.setBackground(Constants.greyColor);		
+		
+		
 		mPrivacyLabel = new JLabel("Privacy: ");
 		mPrivacyLabel.setFont(mFont);
+		mPrivacyLabel.setForeground(Constants.goldColor);
+		mPrivacyLabel.setBackground(Constants.greyColor);		
+
+		
 		mListLabel = new JLabel("List: ");
 		mListLabel.setFont(mFont);
+		mListLabel.setForeground(Constants.goldColor);
+		mListLabel.setBackground(Constants.greyColor);		
+		
+		
 		mDescriptionLabel = new JLabel("Description: ");
 		mDescriptionLabel.setFont(mFont);
+		mDescriptionLabel.setForeground(Constants.goldColor);
+		mDescriptionLabel.setBackground(Constants.greyColor);
+		
+		
 		mSaveButton = new JButton("Save");
 		mSaveButton.setFont(mFont.deriveFont(20));
+		mSaveButton.setForeground(Constants.goldColor);
+		mSaveButton.setBackground(Color.black);
+		
+		
 		mPublicRB = new JRadioButton("Public");
 		mPublicRB.setFont(mFont.deriveFont(20));
 		mPublicRB.setSelected(!to.getIsPrivate());
+		mPublicRB.setForeground(Constants.redColor);
+		mPublicRB.setBackground(Constants.greyColor);		
+		
 		mPrivateRB = new JRadioButton("Private");
 		mPrivateRB.setFont(mFont.deriveFont(20));
 		mPrivateRB.setSelected(to.getIsPrivate());
+		mPrivateRB.setForeground(Constants.redColor);
+		mPrivateRB.setBackground(Constants.greyColor);		
+		
+		
 		mTitleText = new JTextField(15);
 		mTitleText.setText(to.getTitle());
+		mTitleText.setBorder(null);
 		mTitleText.setFont(mFont.deriveFont(20));
+		mTitleText.setForeground(Constants.redColor);
+		mTitleText.setBackground(Constants.lightGreyColor);		
+		
+		
 		mDescriptionText = new JTextArea(5, 15);
+		mDescriptionText.setBorder(null);
 		mDescriptionText.setText(to.getDescription());
-		mDescriptionText.setFont(mFont.deriveFont(0, 10));
+		mDescriptionText.setFont(mFont.deriveFont(0, 14));
 		mDescriptionText.setWrapStyleWord(true);
+		mDescriptionText.setForeground(Constants.redColor);
+		mDescriptionText.setBackground(Constants.lightGreyColor);		
+		
+		
 		mPriorityVector = new Vector<Integer>();
+		
 		mListVector = new Vector<String>();
+		
 		mListIDVector = new Vector<Integer>();
+		
 		for(int i = 10; i > 0; i--){
 			mPriorityVector.addElement(i);
 		}
+		
 		mPointsText = new JTextField(15);
 		mPointsText.setText(Integer.toString(to.getPoints()));
+		mPointsText.setBorder(null);
+		mPointsText.setFont(mFont);
+		mPointsText.setForeground(Constants.redColor);
+		mPointsText.setBackground(Constants.lightGreyColor);
+		
+		
 		for(int i = 0; i < mTU.getTodoLists().size(); i ++){
 			String name;
 			name = mTU.getTodoLists().get(i).getName();
@@ -118,17 +178,37 @@ public class UpdateTodo extends JFrame {
 		mPriorityBox = new JComboBox<Integer>(mPriorityVector);
 		mPriorityBox.setFont(mFont.deriveFont(20));
 		mPriorityBox.setSelectedItem(to.getPriority());
+		mPriorityBox.setForeground(Constants.redColor);
+		mPriorityBox.setBackground(Constants.greyColor);		
+		
+		
 		mListBox = new JComboBox<String>(mListVector);
 		mListBox.setFont(mFont.deriveFont(2));
 		mListBox.setSelectedIndex(currList);
+		mListBox.setForeground(Constants.redColor);
+		mListBox.setBackground(Constants.greyColor);		
+		
+		
 		mMainPanel =  new JPanel();
+		
 		mTitlePanel =  new JPanel();
+		
 		mPrivacyPanel =  new JPanel();
+		
 		mMainPanel.setLayout(new GridLayout(6, 2));
-		mTitlePanel.setLayout(new FlowLayout());
+		mMainPanel.setForeground(Constants.goldColor);
+		mMainPanel.setBackground(Constants.greyColor);		
+		
+		
+		
+		
 		mPrivacyPanel.setLayout(new FlowLayout());
 		mPrivacyPanel.add(mPublicRB);
 		mPrivacyPanel.add(mPrivateRB);
+		mPrivacyPanel.setForeground(Constants.goldColor);
+		mPrivacyPanel.setBackground(Constants.greyColor);		
+		
+		
 		mMainPanel.add(mTitleLabel);
 		mMainPanel.add(mTitleText);
 		mMainPanel.add(mPriorityLabel);
@@ -137,10 +217,8 @@ public class UpdateTodo extends JFrame {
 		mMainPanel.add(mPointsText);
 		mMainPanel.add(mPrivacyLabel);
 		mMainPanel.add(mPrivacyPanel);
-		
 		mMainPanel.add(mListLabel);
 		mMainPanel.add(mListBox);
-
 		mMainPanel.add(mDescriptionLabel);
 		mMainPanel.add(mDescriptionText);
 		
