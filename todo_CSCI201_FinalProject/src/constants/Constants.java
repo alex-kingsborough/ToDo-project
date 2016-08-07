cdff598627c8f796a65cd836c21a2aac97fe41e5
Forrest Dunlap <fdunlap@>
2016-08-06 19:11:42 -0700

    Constants fix.

--- todo_CSCI201_FinalProject/src/constants/Constants.java
+++ todo_CSCI201_FinalProject/src/constants/Constants.java
@@ -10,11 +10,8 @@ public class Constants {
 	public static final String AUTHENTICATED_MESSAGE = "U&P: CORRECT";
 	public static final String NOT_AUTHENTICATED_MESSAGE = "U&P: INCORRECT";
 	public static final String LOGIN_PREFIX = "AUTHENTICATE: ";
-<<<<<<< HEAD
 	public static final Color goldColor = new Color(255, 204, 0);
 	public static final Color redColor = new Color(153, 0, 0);
-=======
 	public static final String GUEST_USER = "GUEST";
->>>>>>> fee0e4529eb20a500a9fad17d524e1be68befbe7
 	
 }
