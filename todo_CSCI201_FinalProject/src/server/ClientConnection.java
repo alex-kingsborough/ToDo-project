5f6c968be71990d290988faf6815af33d1d6967f
Marshall Jacobs <mjjacobs@usc.edu>
2016-08-07 14:34:49 -0700

    trying to commit

--- todo_CSCI201_FinalProject/src/server/ClientConnection.java
+++ todo_CSCI201_FinalProject/src/server/ClientConnection.java
@@ -35,7 +35,7 @@ public class ClientConnection extends Thread{
 			mOutputWriter.writeObject(line);
 			mOutputWriter.flush();
 		} catch (IOException ioe) {
-			System.out.println("ioe: " + ioe.getMessage());
+			System.out.println("ioe in sendMessage: " + ioe.getMessage());
 		}
 	}
 
@@ -108,13 +108,16 @@ public class ClientConnection extends Thread{
 		} else if (s.startsWith(Constants.ADD_FRIEND_REQUEST)){
 			String username = "";
 			if(s.split(" ").length == 2){
-			username = s.split(" ")[1];
-			int uID = Database.get().getUserID(username);
-			if(uID != 0){
-				sendMessage(Constants.SUCCESS_MESSAGE + " " + uID);
-			} else {
-				sendMessage(Constants.FAIL_MESSAGE);
-			}
+				username = s.split(" ")[1];
+				System.out.println("username: " + username);
+				int uID = Database.get().getUserID(username);
+				System.out.println("uID:  " + uID);
+				if(uID != 0){
+					System.out.println(Constants.SUCCESS_MESSAGE + " " + uID);
+					sendMessage(Constants.SUCCESS_MESSAGE + " " + uID);
+				} else {
+					sendMessage(Constants.FAIL_MESSAGE);
+				}
 			} else {
 				sendMessage(Constants.FAIL_MESSAGE);
 			}
